c9ee19b2a450abd350498f4bd3ed903f30b64b4b
csrlima <c.lima.14@hotmail.com>
2018-11-08 11:43:13 -0600

    update

--- www/js/index.js
+++ www/js/index.js
@@ -24,29 +24,38 @@ app.initialize();
 /// DESCARGA DE CONTENIDO
 document.addEventListener("deviceready", function() {
     console.log('ready listener')
-    var url = 'http://xmpp.radiomarketbeat.com/plataforma/assets/lnImagenes/';
-    prepare_url(url, 'useeupoll001.jpg')
-    prepare_url(url, 'useeupoll002.jpg')
-    prepare_url(url, 'useeupoll003.jpg')
-    prepare_url(url, 'useeupoll004.jpg')
-    prepare_url(url, 'useeupoll005.jpg')
-    prepare_url(url, 'useeupoll006.jpg')
-    prepare_url(url, 'useeupoll007.jpg')
-    prepare_url(url, 'useeupoll008.jpg')
+    prepare_url('useeupoll001.jpg')
+    prepare_url('useeupoll002.jpg')
+    prepare_url('useeupoll003.jpg')
+    prepare_url('useeupoll004.jpg')
+    prepare_url('useeupoll005.jpg')
+    prepare_url('useeupoll006.jpg')
+    prepare_url('useeupoll007.jpg')
+    prepare_url('useeupoll008.jpg')
     // window.location = "usee.html"
 }, false);
 
-function prepare_url(url, local_name) {
+function prepare_url(local_name) {
+     window.resolveLocalFileSystemURL("file:///storage/emulated/0/"+local_name, fileExists, fileDoesNotExist);
+}
+
+function fileExists(fileEntry){
+    console.log("Archivo ya existe");
+}
+
+function fileDoesNotExist(){
+    var url = 'http://xmpp.radiomarketbeat.com/plataforma/assets/lnImagenes/';
     window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
-        console.log('file system open: ' + fs.name);
-        fs.root.getFile(local_name, { create: false, exclusive: false }, function (fileEntry) {
-            // if(fileEntry.isFile === false){
-                download(fileEntry, url + local_name, false);
-            // }else{
-            //     console.log("Archivo ya existe: " + local_name);
-            // }
-        }, onErrorCreateFile);
+       console.log('file system open: ' + fs.name);
+       fs.root.getFile(local_name, { create: true, exclusive: false }, function (fileEntry) {
+           // if(fileEntry.isFile === false){
+               download(fileEntry, url + local_name, false);
+           // }else{
+           //     console.log("Archivo ya existe: " + local_name);
+           // }
+       }, onErrorCreateFile);
     }, onErrorLoadFs);
+
 }
 
 function download(fileEntry, uri, readBinaryData) {
