6b01bc49218ea58b783bf2c226e6eeb9b73aabd0
csrlima <c.lima.14@hotmail.com>
2018-11-09 09:32:59 -0600

    Fullscreen, obtener playlist por id

--- www/js/index.js
+++ www/js/index.js
@@ -23,27 +23,46 @@ var app = {
 
 app.initialize();
 
+$(document).ready(function(){
+    //get_playlist_updated_usee()
+})
+
 /// DESCARGA DE CONTENIDO
 document.addEventListener("deviceready", function() {
-    console.log('ready listener')
-    prepare_url('useeupoll001.jpg')
-    prepare_url('useeupoll002.jpg')
-    prepare_url('useeupoll003.jpg')
-    prepare_url('useeupoll004.jpg')
-    prepare_url('useeupoll005.jpg')
-    prepare_url('useeupoll006.jpg')
-    prepare_url('useeupoll007.jpg')
-    prepare_url('useeupoll008.jpg')
-    // window.location = "usee.html"
+    get_playlist_updated_usee()
+    var playlist_usee = json_decode(localStorage.getItem("playlist_usee"));
+    $.each(playlist_usee, function(index, item) {
+        search_file_local(item.nombre_imagen);
+    });
 }, false);
 
-function prepare_url(file_name) {
+function get_playlist_updated_usee() {
+    var data ={
+        'id_app' : id_app,
+        'X-API-KEY': key_value
+    };
+    $.ajax({
+        url: base_url + "usee_services/get_lista",
+        type: "POST",
+        dataType: 'json',
+        data : data,
+        success: function(response) {
+            if(response.status == "success"){
+                console.log(json_encode(response.data));
+                localStorage.setItem("playlist_usee", json_encode(response.data));
+            }else{
+                console.log("Error POST: No se obtuvo la lista de imagenes actualizada");
+            }
+        }
+    });
+}
+
+function search_file_local(file_name) {
     $.ajax({
         url: "file:///storage/emulated/0/" + file_name,
         type: 'HEAD',
         error: function(){
             download_file(file_name)
-            // console.log("file not exists "+file_name);
         },
         success: function(){
             console.log('Archivo ya existe: ' + file_name);
