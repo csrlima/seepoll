f3a849990086258ed7f950781d3334bf5835a167
csrlima <c.lima.14@hotmail.com>
2018-11-09 15:07:14 -0600

    update

--- www/js/index.js
+++ www/js/index.js
@@ -27,15 +27,26 @@ $(document).ready(function(){
     get_playlist_updated_usee()
 })
 
+var pf_local = 0;
+var pf_server = 0;
+
 /// DESCARGA DE CONTENIDO
 function update_files() {
     document.addEventListener("deviceready", function() {
         var playlist_usee = json_decode(localStorage.getItem("playlist_usee"));
         $.each(playlist_usee, function(index, item) {
+            pf_server++;
             search_file_local(item.nombre_imagen);
         });
     }, false);
-    window.location = "usee.html"
+    setInterval('local_wait()',1000);
+
+}
+
+function local_wait(){
+    if(pf_local == pf_server){
+        window.location = "usee.html"
+    }
 }
 
 function get_playlist_updated_usee() {
@@ -68,6 +79,7 @@ function search_file_local(file_name) {
             download_file(file_name)
         },
         success: function(){
+            pf_local++;
             console.log('Archivo ya existe: ' + file_name);
         }
     });
@@ -79,6 +91,7 @@ function download_file(file_name){
     fileTransfer.download(url + file_name, "file:///storage/emulated/0/" + file_name,
         function(entry) {
             console.log("Success!");
+            pf_local++;
         },
         function(err) {
             console.log("Error");
